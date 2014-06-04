2d27284008b89e86174e959d437fff87eaa4fc09
playthisgame <bossman759@yandex.com>
2014-06-04 17:36:06 -0400

    22

--- levels.js
+++ levels.js
@@ -284,8 +284,8 @@ function levels(level){
 		lazer = boxes.create(225, game.world.height - 150, 'lazer');
 		lazer = boxes.create(250, game.world.height - 150, 'lazer');
 		lazer = boxes.create(275, game.world.height - 150, 'lazer');
-		lazer = boxes.create(350, game.world.height - 150, 'lazer');
-		blocks = boxes.create(350, game.world.height - 125, 'block2');
+		lazer = boxes.create(300, game.world.height - 150, 'lazer');
+		blocks = boxes.create(300, game.world.height - 125, 'block2');
 		blocks = boxes.create(450, game.world.height - 125, 'block2');
 		blocks = boxes.create(475, game.world.height - 125, 'block2');
 		blocks = boxes.create(475, game.world.height - 150, 'block2');
