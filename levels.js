4c8efcc7619dc0ac711c70f594b941d5c2e754e5
playthisgame <bossman759@yandex.com>
2014-06-04 18:09:32 -0400

    23

--- levels.js
+++ levels.js
@@ -286,14 +286,14 @@ function levels(level){
 		lazer = boxes.create(275, game.world.height - 150, 'lazer');
 		lazer = boxes.create(300, game.world.height - 150, 'lazer');
 		blocks = boxes.create(300, game.world.height - 125, 'block2');
-		blocks = boxes.create(450, game.world.height - 125, 'block2');
-		blocks = boxes.create(475, game.world.height - 125, 'block2');
-		blocks = boxes.create(475, game.world.height - 150, 'block2');
-		lazer = boxes.create(500, game.world.height - 150, 'lazer');
-		lazer = boxes.create(525, game.world.height - 150, 'lazer');
-		blocks = boxes.create(550, game.world.height - 150, 'block2');
 		blocks = boxes.create(550, game.world.height - 125, 'block2');
 		blocks = boxes.create(575, game.world.height - 125, 'block2');
+		blocks = boxes.create(575, game.world.height - 150, 'block2');
+		lazer = boxes.create(600, game.world.height - 150, 'lazer');
+		lazer = boxes.create(625, game.world.height - 150, 'lazer');
+		blocks = boxes.create(650, game.world.height - 150, 'block2');
+		blocks = boxes.create(650, game.world.height - 125, 'block2');
+		blocks = boxes.create(675, game.world.height - 125, 'block2');
 	}
 	if(level > 23){
 		blocks.kill();	
