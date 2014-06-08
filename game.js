5f0c9cf6bec24d4fcfb0a570060fb55ffbeae70b
playthisgame <bossman759@yandex.com>
2014-06-08 19:11:08 -0400

    122

--- game.js
+++ game.js
@@ -57,7 +57,8 @@
 			scoreText = game.add.text(16, 16, level, { fontSize: '32px', fill: '#ecf0f1' });
 			livesText = game.add.text(16, 48, 'Lives ' + lives, { fontSize: '32px', fill: '#ecf0f1' });
 			saveHighScore();
-			highScoreText = game.add.text(16, 80, 'Best ' + store.get("highScore"), { fontSize: '32px', fill: '#ecf0f1' });
+			var highScore = store.get("highScore");
+			highScoreText = game.add.text(16, 80, 'Best ' + highScore, { fontSize: '32px', fill: '#ecf0f1' });
 
 		}
 		function update() {
