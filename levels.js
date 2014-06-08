771fc9accf1819645e9a65642bb69df4718beeda
playthisgame <bossman759@yandex.com>
2014-06-08 17:39:36 -0400

    94

--- levels.js
+++ levels.js
@@ -328,6 +328,7 @@ function levels(level){
 	}
 	if(level === 27){
 		blocks.kill();
+		blocks = boxes.create(200, game.world.height - 125, 'block2');
 		blocks = boxes.create(300, game.world.height - 125, 'block2');
 		blocks = boxes.create(300, game.world.height - 150, 'block2');
 		lava = boxes.create(325, game.world.height - 125, 'lava');
