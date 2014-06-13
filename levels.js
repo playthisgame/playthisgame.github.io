33472ec3a3b494e9ef7c569285f91df8bc1362cb
playthisgame <bossman759@yandex.com>
2014-06-13 19:13:59 -0400

    146

--- levels.js
+++ levels.js
@@ -352,19 +352,19 @@ function levels(level){
 	}
 	if(level === 28){
 		blocks.kill();
+		blocks = boxes.create(225, game.world.height - 125, 'block2');
 		blocks = boxes.create(250, game.world.height - 125, 'block2');
-		blocks = boxes.create(275, game.world.height - 125, 'block2');
-		blocks = boxes.create(275, game.world.height - 150, 'block2');
+		blocks = boxes.create(250, game.world.height - 150, 'block2');
+		lava = boxes.create(275, game.world.height - 125, 'lava');
 		lava = boxes.create(300, game.world.height - 125, 'lava');
 		lava = boxes.create(325, game.world.height - 125, 'lava');
 		lava = boxes.create(350, game.world.height - 125, 'lava');
-		lava = boxes.create(375, game.world.height - 125, 'lava');
+		lazer = boxes.create(275, game.world.height - 150, 'lazer');
 		lazer = boxes.create(300, game.world.height - 150, 'lazer');
 		lazer = boxes.create(325, game.world.height - 150, 'lazer');
 		lazer = boxes.create(350, game.world.height - 150, 'lazer');
-		lazer = boxes.create(375, game.world.height - 150, 'lazer');
-		blocks = boxes.create(400, game.world.height - 125, 'block2');
-		blocks = boxes.create(400, game.world.height - 150, 'block2');
+		blocks = boxes.create(375, game.world.height - 125, 'block2');
+		blocks = boxes.create(375, game.world.height - 150, 'block2');
 		blocks = boxes.create(600, game.world.height - 125, 'block2');
 		lava = boxes.create(575, game.world.height - 125, 'lava');
 		lava = boxes.create(575, game.world.height - 150, 'lava');
