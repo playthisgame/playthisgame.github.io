2a22d2292dab2bc7805aa1288ace86992ce4d4f2
playthisgame <bossman759@yandex.com>
2014-06-14 18:59:42 -0400

    158

--- game.js
+++ game.js
@@ -80,15 +80,15 @@
 			if(lives === 0){
 				level = 1;
 				lives = 3;
-				game.add.text(game.world.centerX, 80, "You have ran out of lives!", { fontSize: '32px', fill: '#ecf0f1' });
+				game.add.text(game.world.centerX, 200, "You have ran out of lives!", { fontSize: '32px', fill: '#ecf0f1' });
 				if(game.input.mouse.button==0){
 					game.state.start(game.state.current);
 				}
-			}
+			}else
 			loseAudio = game.add.audio('lose');
 			loseAudio.play();
 			game.state.start(game.state.current);
-			
+			}
 		}
 		
 		function levelEnd(){
