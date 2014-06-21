a2a3202158b5797725a9a3e798b558ca17b9a8a2
playthisgame <bossman759@yandex.com>
2014-06-20 22:21:34 -0400

    200!

--- levels.js
+++ levels.js
@@ -396,12 +396,12 @@ function levels(level){
 		lazer = boxes.create(600, game.world.height - 125, 'lazer');
 		lazer = boxes.create(625, game.world.height - 125, 'lazer');
 		blocks = boxes.create(650, game.world.height - 125, 'block2');
-		blocks = boxes.create(550, game.world.height - 225, 'block2');
-		blocks = boxes.create(575, game.world.height - 225, 'block2');
-		blocks = boxes.create(600, game.world.height - 225, 'block2');
-		blocks = boxes.create(550, game.world.height - 250, 'block2');
-		blocks = boxes.create(600, game.world.height - 250, 'block2');
-		lava = boxes.create(575, game.world.height - 250, 'lava');
+		blocks = boxes.create(550, game.world.height - 237, 'block2');
+		blocks = boxes.create(575, game.world.height - 237, 'block2');
+		blocks = boxes.create(600, game.world.height - 237, 'block2');
+		lava = boxes.create(550, game.world.height - 262, 'lava');
+		lava = boxes.create(575, game.world.height - 262, 'lava');
+		lava = boxes.create(600, game.world.height - 262, 'lava');
 	}
 	if(level > 29){
 		blocks.kill();	
