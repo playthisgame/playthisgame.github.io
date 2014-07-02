1f0ea0a9827a27e45b3e36664235e4a9bfe103db
playthisgame <bossman759@yandex.com>
2014-07-01 20:03:38 -0400

    231

--- levels.js
+++ levels.js
@@ -10,6 +10,7 @@ function levels(level){
 	lazer.body.immovable = true;
 	fallingBoxes = game.add.group();
 	var falling = fallingBoxes.create(0, game.world.height - 0, 'block2');
+	falling.body.velocity.y = 150;
 	if(level == 1){
 		game.add.text(187, 200, "Blockenstein", { fill: '#FFFFFF', font:'64px Arial'});
 		blocks = boxes.create(400, game.world.height - 125, 'block2');
