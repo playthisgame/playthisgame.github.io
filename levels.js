83c08a2083b4a396b6a2c01863ca1ec73ff6825d
playthisgame <bossman759@yandex.com>
2014-07-03 21:18:53 -0400

    290

--- levels.js
+++ levels.js
@@ -486,17 +486,17 @@ function levels(level){
 	}
 	if(level === 36){
 		blocks.kill();
-		blocks = boxes.create(200, game.world.height - 125, 'block2');
-		blocks = boxes.create(225, game.world.height - 125, 'block2');
-		blocks = boxes.create(325, game.world.height - 125, 'block2');
-		blocks = boxes.create(325, game.world.height - 150, 'block2');
-		blocks = boxes.create(450, game.world.height - 150, 'block2');
-		blocks = boxes.create(475, game.world.height - 150, 'block2');
-		blocks = boxes.create(600, game.world.height - 125, 'block2');
-		blocks = boxes.create(625, game.world.height - 150, 'block2');	
-		blocks = boxes.create(625, game.world.height - 125, 'block2');
-		blocks = boxes.create(650, game.world.height - 150, 'block2');
-		blocks = boxes.create(650, game.world.height - 175, 'block2');	
+		blocks = boxes.create(200, game.world.height - 125, 'emerald');
+		blocks = boxes.create(225, game.world.height - 125, 'emerald');
+		blocks = boxes.create(325, game.world.height - 125, 'emerald');
+		blocks = boxes.create(325, game.world.height - 150, 'emerald');
+		blocks = boxes.create(450, game.world.height - 150, 'emerald');
+		blocks = boxes.create(475, game.world.height - 150, 'emerald');
+		blocks = boxes.create(600, game.world.height - 125, 'emerald');
+		blocks = boxes.create(625, game.world.height - 150, 'emerald');	
+		blocks = boxes.create(625, game.world.height - 125, 'emerald');
+		blocks = boxes.create(650, game.world.height - 150, 'emerald');
+		blocks = boxes.create(650, game.world.height - 175, 'emerald');	
 	}
 	if(level > 35){
 		blocks.kill();	
