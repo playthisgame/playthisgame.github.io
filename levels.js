60cf98fba2e69efc38443bc240c27c7977da0608
playthisgame <bossman759@yandex.com>
2014-07-06 18:47:25 -0400

    314

--- levels.js
+++ levels.js
@@ -553,11 +553,11 @@ function levels(level){
 		blocks = boxes.create(350, game.world.height - 125, 'purple');
 		blocks = boxes.create(350, game.world.height - 150, 'purple');
 		blocks = boxes.create(375, game.world.height - 125, 'purple');
-		blocks = boxes.create(425, game.world.height - 125, 'purple');
-		blocks = boxes.create(450, game.world.height - 125, 'blue');
-		blocks = boxes.create(475, game.world.height - 125, 'blue');
-		blocks = boxes.create(500, game.world.height - 125, 'blue');
-		blocks = boxes.create(525, game.world.height - 125, 'purple');
+		blocks = boxes.create(475, game.world.height - 125, 'purple');
+		blocks = boxes.create(525, game.world.height - 125, 'blue');
+		blocks = boxes.create(550, game.world.height - 125, 'blue');
+		blocks = boxes.create(575, game.world.height - 125, 'blue');
+		blocks = boxes.create(600, game.world.height - 125, 'purple');
 	}
 	if(level > 41){
 		blocks.kill();	
