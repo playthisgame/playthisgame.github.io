0fd694eb65c9fb0f3498b633e3f17e6ed75888e6
playthisgame <bossman759@yandex.com>
2014-07-07 20:17:04 -0400

    321

--- levels.js
+++ levels.js
@@ -573,15 +573,13 @@ function levels(level){
 		blocks = boxes.create(325, game.world.height - 125, 'blue');
 		blocks = boxes.create(350, game.world.height - 125, 'purple');
 		blocks = boxes.create(375, game.world.height - 125, 'purple');
-		blocks = boxes.create(500, game.world.height - 125, 'purple');
-		blocks = boxes.create(525, game.world.height - 125, 'blue');
-		blocks = boxes.create(525, game.world.height - 150, 'purple');
-		blocks = boxes.create(525, game.world.height - 175, 'purple');
-		blocks = boxes.create(550, game.world.height - 150, 'purple');
-		blocks = boxes.create(550, game.world.height - 175, 'purple');
+		blocks = boxes.create(525, game.world.height - 125, 'purple');
 		blocks = boxes.create(550, game.world.height - 125, 'blue');
+		blocks = boxes.create(575, game.world.height - 150, 'purple');
+		blocks = boxes.create(575, game.world.height - 175, 'purple');
 		blocks = boxes.create(575, game.world.height - 125, 'blue');
-		blocks = boxes.create(600, game.world.height - 125, 'purple');
+		blocks = boxes.create(600, game.world.height - 125, 'blue');
+		blocks = boxes.create(625, game.world.height - 125, 'purple');
 		
 	}
 	if(level > 42){
