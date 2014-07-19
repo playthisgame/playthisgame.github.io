5dd6296f536a85a8322e74cb1285a4abe971eba9
playthisgame <bossman759@yandex.com>
2014-07-19 19:26:01 -0400

    420

--- levels.js
+++ levels.js
@@ -803,6 +803,6 @@ function levels(level){
 		blocks = boxes.create(500, game.world.height - 150, 'emerald');
 		blocks = boxes.create(525, game.world.height - 150, 'emerald');
 		blocks = boxes.create(550, game.world.height - 150, 'emerald');
-		blocks = boxes.create(600, game.world.height - 125, 'emerald');
+		blocks = boxes.create(650, game.world.height - 125, 'emerald');
 	}			
 }
