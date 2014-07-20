604c76659725e97f4f4ecba8df0cf6d74640ade4
playthisgame <bossman759@yandex.com>
2014-07-20 19:40:47 -0400

    425

--- levels.js
+++ levels.js
@@ -838,4 +838,12 @@ function levels(level){
 		blocks = boxes.create(650, game.world.height - 125, 'block2');
 		blocks = boxes.create(650, game.world.height - 150, 'block2');
 	}
+	if(level === 66){
+		blocks = boxes.create(200, game.world.height - 125, 'emerald');
+		blocks = boxes.create(225, game.world.height - 150, 'emerald');
+		blocks = boxes.create(250, game.world.height - 125, 'emerald');
+		blocks = boxes.create(250, game.world.height - 150, 'emerald');
+		blocks = boxes.create(250, game.world.height - 175, 'emerald');
+		
+	}
 }
