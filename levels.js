1959e66df0f93e8fb7f81fb65b6f1f6118fb7cc9
playthisgame <bossman759@yandex.com>
2014-07-29 19:42:55 -0400

    490

--- levels.js
+++ levels.js
@@ -1085,7 +1085,10 @@ function levels(level){
 		blocks = boxes.create(300, game.world.height - 125, 'purple');
 		blocks = boxes.create(350, game.world.height - 150, 'purple');
 		blocks = boxes.create(400, game.world.height - 125, 'purple');
-		blocks = boxes.create(200, game.world.height - 125, 'purple');
+		blocks = boxes.create(600, game.world.height - 150, 'purple');
+		blocks = boxes.create(600, game.world.height - 175, 'purple');
+		blocks = boxes.create(700, game.world.height - 125, 'purple');
+		blocks = boxes.create(700, game.world.height - 150, 'purple');
 	}
 	if(level === 86){
 		blocks = boxes.create(200, game.world.height - 125, 'purple');
