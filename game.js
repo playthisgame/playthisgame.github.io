ec87b041e332fe5ee2f72ccd3685962dd2b83ddf
playthisgame <bossman759@yandex.com>
2014-08-02 20:19:38 -0400

    607

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ var lives = 3;
 			this.scale.maxHeight = 600;
 		}
 		this.scale.pageAlignVertically = true;
+		this.scale.pageAlignHorizontally = true;
 		game.scale.refresh();
 	
 		game.physics.startSystem(Phaser.Physics.ARCADE);			
