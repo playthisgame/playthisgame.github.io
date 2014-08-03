ab9d1f41862d6f7ad33f07dea3418147d7c1c6a8
playthisgame <bossman759@yandex.com>
2014-08-03 19:02:31 -0400

    642

--- game.js
+++ game.js
@@ -20,8 +20,8 @@ var lives = 3;
 	function create() {
 		// scale game for mobile devices
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.minHeight = 480;
-		this.scale.maxHeight = 600;
+		this.scale.maxWidth = 320;
+		this.scale.maxHeight = 480;
 		this.scale.pageAlignHorizontally = true;
 		game.scale.refresh();
 	
