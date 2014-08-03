feff6e375fe9a3e5cfb76bb05a09295d03ecd3ae
playthisgame <bossman759@yandex.com>
2014-08-02 20:00:03 -0400

    597

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ var lives = 3;
 		this.scale.minWidth = 320;
 		this.scale.minHeight = 480;
 		if(screen.width <= 540){
-			this.scale.maxWidth = 320;
+			this.scale.maxWidth = 360;
 			this.scale.maxHeight = screen.height;			
 		}else{
 			this.scale.maxWidth = 750;
