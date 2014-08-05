a4bd76d77ef2e454848a5f4dd0700636d191a6b6
playthisgame <bossman759@yandex.com>
2014-08-04 21:35:21 -0400

    651

--- levels.js
+++ levels.js
@@ -1226,4 +1226,9 @@ function levels(level){
 		blocks = boxes.create(650, game.world.height - 150, 'block2');
 		blocks = boxes.create(650, game.world.height - 125, 'block2');
 	}
+	if(level > 100){
+		store.clear();
+		level = 1;
+		game.state.start(game.state.current);
+	}
 }
